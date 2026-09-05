3dbd75459d95cd99fba5e62688c2b2e169f7763c
miris-mp <miriam.petrocchi94@gmail.com>
2026-09-05 11:28:12 +0200

    Add goto, edit layout, update menu and change map

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,28 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    optimizeDeps: {
-      exclude: ["maplibre-gl"],
-    },
   },
 });
+
+// // @ts-check
+// import { defineConfig } from "astro/config";
+
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   vite: {
+//     plugins: [tailwindcss()],
+//     optimizeDeps: {
+//       exclude: ["maplibre-gl"],
+//     },
+//   },
+//   i18n: {
+//     locales: ["en", "it"], // Locales you want to support
+//     defaultLocale: "en", // Default locale (fallback)
+
+//     routing: {
+//       prefixDefaultLocale: true, // Ensures that your default locale is prefixed aswell
+//     },
+//   },
+// });
